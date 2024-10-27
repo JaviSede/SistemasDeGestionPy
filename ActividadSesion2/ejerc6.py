abdaa5e035df9a6a0383a3e9b826ddf8b73175dc
JaviSede <javisede7@gmail.com>
2024-10-27 16:36:30 +0100

    se hasta el ejercicio8 de la Actividad de la sesion 2

---
+++ ActividadSesion2/ejerc6.py
@@ -0,0 +1,4 @@
+lista = map(lambda x: x * 10, filter(lambda x : x % 2 == 0, (range(1,11))))
+
+print(list(lista))
+
---
+++ ActividadSesion2/ejerc7.py
@@ -0,0 +1,5 @@
+def mostrar_todo(*args, **kwargs):
+    for x in args:
+        print(x)
+    for x in kwargs:
+        print(x)
