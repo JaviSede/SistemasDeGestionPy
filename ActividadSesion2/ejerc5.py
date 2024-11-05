bfb049fd9e55bc69364ab8c3edcfe13740744900
JaviSede <javisede7@gmail.com>
2024-11-05 20:08:41 +0100

    se actualiza el ejercicio5

--- ActividadSesion2/ejerc5.py
+++ ActividadSesion2/ejerc5.py
@@ -3,7 +3,7 @@ lista = (range(1, 11))
 impares = [x for x in lista if x % 2 != 0]
 print(list(impares))
 
-cuadrados = {num: num ** 2 for num in range(1, 11)}
+cuadrados = {num: num ** 2 for num in lista}
 print(cuadrados)
 
 unicos = {x for x in "Pythonista"}
